4733c6c22b9665c6f6e9998e8bc864904f6dcf43
Asm Shuvo <shuvo.sust.cse@gmail.com>
2024-02-14 00:56:00 +0600

    part 1 done

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,4 +1,16 @@
+function continueGame(){
+    const alphabet = getRandomAlphabate();
+    // change icon alphabet
+    const curAlphabetElement = document.getElementById('current-alphabet');
+    curAlphabetElement.innerText = alphabet;
+    setBackgroundColorById(alphabet);
+}
+
 function play(){
     hideElementByID('home-screen');
     showElementByID('play-ground');
+    continueGame();
+
 }
+
+
--- scripts/utility.js
+++ scripts/utility.js
@@ -7,3 +7,27 @@ function showElementByID(id){
     const elem = document.getElementById(id);
     elem.classList.remove('hidden');
 }
+
+function getRandomAlphabate(){
+    const alphabateString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabatesArray = alphabateString.split('');
+    const index = getRandomIndex();
+    const randomAlphabate = alphabatesArray[index];
+
+    return randomAlphabate;
+    
+}
+
+function getRandomIndex(){
+    const index = Math.floor(Math.random() *25);
+    return index;
+}
+
+function setBackgroundColorById(id){
+    const elem = document.getElementById(id);
+    elem.classList.add('bg-orange-400');
+}
+function removeBachgroundColorById(id){
+    const elem = document.getElementById(id);
+    elem.classList.remove('bg-orange-400');
+}
