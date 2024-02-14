d03d1c353c5c27a33552deae3ab0778014e987f1
Asm Shuvo <shuvo.sust.cse@gmail.com>
2024-02-15 02:47:52 +0600

    score and life activated

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,4 +1,3 @@
-
 function handleKeyboardKeyupEvent(e){
     // the alphabet pressed
     const pressedKey = e.key;
@@ -6,12 +5,32 @@ function handleKeyboardKeyupEvent(e){
     const curAlphabetElement = document.getElementById('current-alphabet');
     const curAlphabet = curAlphabetElement.innerText;
     expectedAlphabet = curAlphabet.toLowerCase();
+    
+
     //check matched or not
     if(pressedKey==expectedAlphabet){
         console.log("You got a point!!!");
+        
+        // update score:
+        const curScoreElem = document.getElementById('current-score');
+        const curScoreElemText = curScoreElem.innerText;
+        const curScore = parseFloat(curScoreElemText);
+        const newScore = curScore+1;
+        curScoreElem.innerText=newScore;
+
+        // start new round:
+        removeBachgroundColorById(expectedAlphabet);
+        continueGame();
     }
     else{
         console.log("you missed. You lost a life");
+
+        // update score:
+        const curLifeElem = document.getElementById('current-life');
+        const curLifeElemText = curLifeElem.innerText;
+        const curLife = parseFloat(curLifeElemText);
+        const newLife = curLife-1;
+        curLifeElem.innerText=newLife;
     }
 }
 
